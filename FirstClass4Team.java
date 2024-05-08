e23dc8ece788e91c5eb8e61586a5716d8f0d97dd
owen0806 <owen080614@gmail.com>
2024-05-08 16:03:04 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -5,6 +5,9 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");//jqowdjqwdo
         System.out.println("\"學號：\"01157122\"\n姓名：\"胡智涵\"");
+        System.out.println("\"學號：\"01157111\"\n姓名：\"林彥丞\"");
+        System.out.println("\"學號：\"01257133\"\n姓名：\"蘇東毅\" 請假");
+
     }
     
 }
