77ef1a951af5a7bde02279d056ed959050bd074b
Ateto1202 <tonysu1204@gmail.com>
2024-05-08 17:09:28 +0800

    Update FirstClass4Team.java 我是資工 1B 蘇東毅，學號 01257133。

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -6,7 +6,7 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");//jqowdjqwdo
         System.out.println("\"學號：\"01157122\"\n姓名：\"胡智涵\"");
         System.out.println("\"學號：\"01157111\"\n姓名：\"林彥丞\"");
-        System.out.println("\"學號：\"01257133\"\n姓名：\"蘇東毅\" 請假");
+        System.out.println("\"學號：\"01257133\"\n姓名：\"蘇東毅\"");
 
     }
     
